8e4dff0746b0fbd05fea590e4d63c1c0fd59d5c6
Ola-22 <olanazly@gmail.com>
2022-02-13 19:26:08 +0200

    Add pages to the Router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import "./App.css";
-// import Footer from "./Components/Footer";
-// import Search from "./Components/Search";
-// import ServiceProduct from "./Components/ServiceProduct";
 import DataProvider from "./Context/DataProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeServices from "./pages/HomeServices";
 import HomePage from "./pages/HomePage";
+import HomeDetails from "./pages/HomeDetails";
+import Services from "./pages/Services";
+import ServiceCleaning from "./pages/ServiceCleaning";
 
 function App() {
   return (
     <DataProvider>
       <div className="App">
         <BrowserRouter>
-          {/* <Search /> */}
           <Routes>
             <Route path="/home" element={<HomeServices />} />
+            <Route path="/home/:id" element={<HomeDetails />} />
+            <Route path="/home/:id/:id" element={<Services />} />
+            <Route path="/home/:id/:id/:id" element={<ServiceCleaning />} />
+
             <Route path="/" element={<HomePage />} />
           </Routes>
-          {/* <img src="/images/banner.png" alt="" style={{ marginLeft: "16px" }} />
-          <div>
-            <ServiceProduct />
-          </div>
-          <Footer /> */}
         </BrowserRouter>
       </div>
     </DataProvider>
