e5ccaab4a8023e0639dee54e6b4db8162f07e76f
Ola-22 <olanazly@gmail.com>
2022-02-14 20:34:35 +0200

    Add search function to filter services data

--- src/pages/ServiceCleaning.js
+++ src/pages/ServiceCleaning.js
@@ -1,5 +1,5 @@
 import SearchPages from "../Components/SearchPages";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../Context/DataProvider";
 import { useParams } from "react-router-dom";
 import Card from "../Components/Card";
@@ -11,6 +11,16 @@ export default function ServiceCleaning() {
   const value = useContext(DataContext);
   const [services] = value.services;
 
+  const [searchService, setSearchService] = useState([]);
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    const newFilter = services.HomeServices.filter((val) => {
+      return val.ServiceOne.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    setSearchService(newFilter);
+  };
+
   const { id } = useParams();
 
   const Details = services.HomeServices[0].serviceClean.filter(
@@ -19,26 +29,70 @@ export default function ServiceCleaning() {
     }
   );
   return (
-    <div>
-      <SearchPages imgSrc="/images/Home.png" />
-      {Details.map((service) => (
-        <div key={service._id}>
-          <Link to={`/home/${service._id}/${service._id}/${service._id}`}>
-            <div className="card-pages">
-              <Card
-                // 189
-                imgService={service.img}
-                title={service.title}
-                ratings
-                rating={service.Rating}
-                paragraph={service.paragraph}
-                price={service.price}
-                review={service.review}
-              />
+    <div className="service-cleaning">
+      <SearchPages imgSrc="/images/Home.png" onChange={handleFilter} />
+      <div className="services">
+        {searchService.length !== 0 && (
+          <div className="service-product">
+            <div
+              className="Group-container"
+              style={{ marginTop: "2em", justifyContent: "flex-start" }}
+            >
+              <>
+                {searchService.map((service) => (
+                  <div>
+                    <Link to={`/home/${service._id}/${service._id}`}>
+                      <Card
+                        imgService={service.img}
+                        title={service.title}
+                        ratings
+                        rating={service.Rating}
+                        paragraph={service.paragraph}
+                        price={service.price}
+                        review={service.review}
+                      />
+                    </Link>
+                  </div>
+                ))}
+              </>
             </div>
-          </Link>
+          </div>
+        )}
+        {searchService.length === 0 && (
+          <>
+            {Details.map((service) => (
+              <div key={service._id}>
+                <Link to={`/home/${service._id}/${service._id}/${service._id}`}>
+                  <div className="card-pages" style={{ marginTop: "17px" }}>
+                    <Card
+                      imgService={service.img}
+                      title={service.title}
+                      ratings
+                      rating={service.Rating}
+                      paragraph={service.paragraph}
+                      price={service.price}
+                      review={service.review}
+                    />
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </>
+        )}
+        <div className="pick-time">
+          <h6>Pick time & date:</h6>
+          <div style={{ display: "flex" }}>
+            <input value="DD/MM/" type="date" />
+            <input type="time" />
+          </div>
+
+          <textarea name="w3review">Notes</textarea>
         </div>
-      ))}
+        <Link to="/request">
+          <button className="requestBtn">Request</button>
+        </Link>
+      </div>
+
       <div style={{ position: "fixed", width: "375px", bottom: "0" }}>
         <Footer />
       </div>
