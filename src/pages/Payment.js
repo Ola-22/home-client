4e0beceb1e04ba8c16eb334f106038ab9bcbf0f7
Ola-22 <olanazly@gmail.com>
2022-02-15 10:25:36 +0200

    Add input phone, and payment button

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -3,6 +3,7 @@ import Footer from "../Components/Footer";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { CardCvcElement } from "@stripe/react-stripe-js";
+import { Link } from "react-router-dom";
 
 const promise = loadStripe(
   "pk_test_51IB0LMCyqqGmV68NMrqVLQ8ll4WzSXGAsEKDU74eUrCPexxwtfC2dSmyFlnsAZExeVpJeYTAMxvrahdROwMekaLp00lRScO2Uv"
@@ -13,7 +14,21 @@ export default function Payment() {
     <Elements stripe={promise}>
       <div>
         <SearchPages imgSrc="/images/Home.png" />
-        <CardCvcElement />
+        <div className="services">
+          <div className="mobile-box">
+            <h6>Service request details:</h6>
+            <div>
+              <img width="11" height="17" src="/images/phone.png" alt="" />
+              <input type="tel" placeholder="Mobile Number" />
+            </div>
+            <h6>Payment details:</h6>
+            <div className="btn-payment">
+              <button>Online Payment</button>
+              <button>Cash</button>
+            </div>
+            {/* <CardCvcElement /> */}
+          </div>
+        </div>
 
         <div style={{ position: "fixed", width: "375px", bottom: "0" }}>
           <Footer />
