8ef3d4ca15a82cf037fc70bf28d5cd3fb1926ee6
Ola-22 <olanazly@gmail.com>
2022-02-16 14:45:55 +0200

    Edit search filter

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,32 +1,14 @@
-import { useContext, useState } from "react";
-import { DataContext } from "../../Context/DataProvider";
 import "./style.css";
 
-export default function Search() {
-  const [searchService, setSearchService] = useState([]);
-  const value = useContext(DataContext);
-  const [services] = value.services;
-
-  const handleFilter = (e) => {
-    const searchWord = e.target.value;
-    const newFilter = services.NewServices.filter((val) => {
-      return val.title.toLowerCase().includes(searchWord.toLowerCase());
-    });
-    setSearchService(newFilter);
-  };
-
+export default function Search({ onChange }) {
   return (
     <div className="search">
       <img src="/images/Filter.png" alt="" />
       <input
-        onChange={handleFilter}
+        onChange={onChange}
         type="text"
         placeholder="Search for a service"
       />
-      {searchService.length !== 0 &&
-        searchService.map((service) => (
-          <h2 key={service.id}>{service.title}</h2>
-        ))}
 
       <img src="/images/Voice.png" alt="" />
     </div>
