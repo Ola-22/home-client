13ac70aa46a848c1192ae5ed29fc1f5bc2cf255d
Ola-22 <olanazly@gmail.com>
2022-02-20 10:48:11 +0200

    Edit props of the card in service cleaning page

--- src/pages/ServiceCleaning.js
+++ src/pages/ServiceCleaning.js
@@ -49,6 +49,7 @@ export default function ServiceCleaning() {
                       imgLocation=""
                       rateImg=""
                       rateVal=""
+                      currency={service.currency}
                     />
                   </div>
 
@@ -86,6 +87,7 @@ export default function ServiceCleaning() {
                     imgLocation=""
                     rateImg=""
                     rateVal=""
+                    currency={service.currency}
                   />
                 </div>
 
